ea9051c98363bbc1b54a4d63f1925ebe582d881a
KoheiNaoe <naoe.acial@gmail.com>
2022-01-20 02:46:19 +0900

    2-1

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,9 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
-Route::get('hello', function () {
-    return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+Route::get('hello/{msg}', function ($msg='no message.') {
+
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body { font-size: 16pt; color: #999; }
+h1 { font-size: 100pt; text-align: right; color: #eee; margin: -40px 0px -50px 0px; }
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>{$msg}</p>
+    <p>これは、サンプルページで作ったページです。</p>
+    </body>
+</html>
+EOF;
+
+	return $html;
 });
