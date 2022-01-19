508a1969be6b916aea7fa7041883f25b3adcd592
KoheiNaoe <naoe.acial@gmail.com>
2022-01-20 02:45:48 +0900

    設定ファイル修正

--- laravel/webpack.mix.js
+++ laravel/webpack.mix.js
@@ -12,6 +12,16 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .postCss('resources/css/app.css', 'public/css')
+
+    // browserSyncの設定
+    .browserSync({
+        files: [
+            'resources/**/*',
+            'app/**/*',
+            'config/**/*',
+            'routes/**/*',
+            'public/**/*',
+        ],
+        proxy: 'http://localhost:80',
+    });
