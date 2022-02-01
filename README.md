6e8a75f7b3de9f1a62cef33c500090d11bcab570
KoheiNaoe <naoe.acial@gmail.com>
2022-02-02 00:34:12 +0900

    validator

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 # service provider作成コマンド
 `php artisan make:provider xxx`
 
+# ミドルウェア作成コマンド
+`php artisan make:middleware xxx`
+
 # permission変更
 `chown -R www-data:www-data /var/www`
 
--- laravel/app/Http/Controllers/HelloController.php
+++ laravel/app/Http/Controllers/HelloController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
+use Illuminate\Support\Facades\Validator;
 
 class HelloController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('hello.index', ['message' => 'Hello!']);
+        return view('hello.index', ['msg' => 'フォームを入力して下さい。']);
+    }
+
+    public function post(HelloRequest $request)
+    {
+        return view('hello.index', ['msg' => '正しく入力されました!']);
     }
 }
